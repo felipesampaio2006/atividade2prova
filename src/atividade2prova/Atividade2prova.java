5e1c47936d77ed950dfb160da75159a4dbdf13a1
felipe Sampaio <samlipe2006@gmail.com>
2021-05-04 22:19:12 -0300

    projeto criado dia 04/05/2021 - atividade 2 da prova

---
+++ src/atividade2prova/Atividade2prova.java
@@ -0,0 +1,18 @@
+package atividade2prova;
+
+import static java.lang.Math.PI;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Atividade2prova {
+    public static void main(String[] args) {
+        double x, n1 = 1, n2 = 2 ;
+        Scanner ler = new Scanner(System.in);
+                
+        x = (n1+n2)/PI + 2;
+    }
+    
+}
